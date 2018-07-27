3e0c98518a4c4d10468f21b8741a1becd14f3ac1
Tomas Cicman <tcicman@sygic.com>
2018-07-27 11:55:39 +0200

    delete

--- documentation-demos/raster-maps-demo/demo.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-	var map = L.map('map');
-	L.TileLayer.sygic('nbqm8YwdrLgg396jr85qLPjBe').addTo(map);
-
-	map.setView([40.7667, -73.9780], 14); // Hello Central Park!
-};
